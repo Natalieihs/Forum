0465211a613c0b5a1e936b91284e5498e296b1dc
490158859 <490158859@qq.com>
2018-12-13 12:22:07 +0800

    111

---
+++ Demo/Services/IOnlineUsersService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Models;
+namespace Demo.Services
+{
+    public interface IOnlineUsersService
+    {
+        Task<bool> AddOnlineUser(bma_onlineusers onlineuser);
+
+        Task<bool> GetOnlineusersBySid(string sid);
+    }
+}
---
+++ Demo/Services/OnlineUsersService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Models;
+
+namespace Demo.Services
+{
+    public class OnlineUsersService : DbContext, IOnlineUsersService
+    {
+        private readonly IHttpContextAccessor _httpcontext;
+        public OnlineUsersService(IHttpContextAccessor httpContext)
+        {
+            _httpcontext = httpContext;
+        }
+        public async Task<bool> AddOnlineUser(bma_onlineusers onlineuser)
+        {
+         return  await  db.Insertable(new bma_onlineusers
+            {
+                ip = onlineuser.ip,
+                nickname = onlineuser.nickname,
+                olid = 0,
+                regionid = 0,
+                sid = onlineuser.sid,
+                uid = 0,
+                updatetime = DateTime.Now
+            }).ExecuteCommandAsync()>0;
+        }
+
+        public async Task<bool> GetOnlineusersBySid(string sid)
+        {
+           var users =await db.Queryable<bma_onlineusers>().Where(c => c.sid == sid).SingleAsync();
+            return users == null;
+        }
+    }
+}
